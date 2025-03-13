cf309efcc6119837761af145088d1b43f07392fc
mr-taifur <taifurshishir123@gmail.com>
2025-03-14 00:14:52 +0600

    to notification and cart icon

--- lib/screeens/home/page/home.dart
+++ lib/screeens/home/page/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import '../../../utils/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -12,9 +14,20 @@ class Home extends StatelessWidget {
           style:TextStyle(
           fontFamily: "DMSans",
           fontWeight: FontWeight.bold,
+          color: FColors.oceanBlue,
           ),
         ),
         centerTitle: true,
+        actions: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(Iconsax.notification_bing_outline),
+            ),
+            Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(Icons.shopping_cart),
+            ),
+        ],
       ),
     );
   }
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  //Primary
+  static const primary = Color(0XFF5CC936);
+  static const primaryOrange = Color(0XFFFFC120);
+  static const primaryNavy = Color(0XFF0C1A30);
+  static const primaryWhite = Color(0XFFFFFFFF);
+
+  //Secondary
+  static const secondary = Color(0XFF3A9B7A);
+  static const secondaryRed = Color(0XFFC93545);
+  static const darkGrey = Color(0XFF838589);
+  static const halfGrey = Color(0XFFC4C5C4);
+  static const softGrey = Color(0XFFEDEDED);
+  static const offGrey = Color(0XFFFAFAFA);
+
+  //lebel Colours
+  static const offBlue = Color(0XFFEFF5FB);
+  static const offGreen = Color(0XFFEEFAF6);
+  static const offRed = Color(0XFFFCECEF);
+
+  //Font Colors
+  static const oceanBlue = Color(0XFF3669C9);
+  static const dark = Color(0XFF000000);
+  
+
+
+}
