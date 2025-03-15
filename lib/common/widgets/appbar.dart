cca5adff575a69e44db551c4012d8c675029da5b
mr-taifur <taifurshishir123@gmail.com>
2025-03-15 10:43:15 +0600

    SeparetedAppbar

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -5,32 +5,41 @@ import 'package:icons_plus/icons_plus.dart';
 class FAppBar extends StatelessWidget implements PreferredSizeWidget {
   const FAppBar({
     super.key,
+    required this.title,
+    this.firstIcon = Iconsax.notification_bing_outline,
+    this.secondIcon = Icons.shopping_cart,
+    this.backButton = false,
   });
 
-@override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  final String title;
+  final IconData firstIcon;
+  final IconData secondIcon;
+  final bool backButton;
 
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: backButton ? Icon(Iconsax.arrow_left_2_outline) : null,
       title: Text(
-        "Mega Shop",
-        style:TextStyle(
-        fontFamily: "DMSans",
-        fontWeight: FontWeight.bold,
-        color: FColors.oceanBlue,
+        title,
+        style: TextStyle(
+          fontFamily: "DMSans",
+          fontWeight: FontWeight.bold,
+          color: FColors.oceanBlue,
         ),
       ),
       centerTitle: true,
       actions: [
         Container(
-          padding: EdgeInsets.all(10),
-          child: Icon(Iconsax.notification_bing_outline),
-          ),
-          Container(
-          padding: EdgeInsets.all(10),
-          child: Icon(IonIcons.cart),
-          ),
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(firstIcon, color: FColors.dark),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+          child: Icon(secondIcon, color: FColors.dark),
+        ),
       ],
     );
   }
